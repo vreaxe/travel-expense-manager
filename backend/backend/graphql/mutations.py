3c904dc3a053c97edbdd7da5d7d3e4137adb052f
vreaxe <vreaxe@gmail.com>
2019-05-09 20:44:58 +0200

    Add BaseMutation
    Based in Saleor's BaseMutation

---
+++ backend/backend/graphql/mutations.py
@@ -0,0 +1,34 @@
+import graphene
+from graphene.types.mutation import MutationOptions
+from django.core.exceptions import ValidationError as DjangoValidationError
+
+from backend.graphql.exceptions import PermissionDenied
+from backend.graphql.exceptions import ValidationError
+
+# Based on BaseMutation: https://github.com/mirumee/saleor/blob/master/saleor/graphql/core/mutations.py
+class BaseMutation(graphene.Mutation):
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def __init_subclass_with_meta__(
+        cls,
+        arguments=None,
+        description=None,
+        _meta=None,
+        **options
+    ):
+        if not _meta:
+            _meta = MutationOptions(cls)
+        super().__init_subclass_with_meta__(_meta=_meta, **options)
+
+    @classmethod
+    def mutate(cls, root, info, **data):
+        try:
+            return cls.perform_mutation(root, info, **data)
+        except DjangoValidationError as django_validation_error:
+            raise ValidationError(form_errors=django_validation_error.message_dict)
+
+    @classmethod
+    def perform_mutation(cls, root, info, **data):
+        pass
