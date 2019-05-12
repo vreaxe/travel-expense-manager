1edf4ca6684b16dd858374076ff0b09771900d91
vreaxe <vreaxe@gmail.com>
2019-05-12 16:26:28 +0200

    CSRF exempt of GraphQL url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.decorators.csrf import csrf_exempt
 
 from backend.graphql.view import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
 if settings.DEBUG:
