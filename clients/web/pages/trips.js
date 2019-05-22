3f0601f9879bd24e5a12f26eb0ecb407a39cb39f
vreaxe <vreaxe@gmail.com>
2019-05-22 23:04:29 +0200

    Cleanup

--- clients/web/pages/trips.js
+++ clients/web/pages/trips.js
@@ -1,16 +1,17 @@
-import { Link } from "../routes";
 import withAuth from "../lib/withAuth";
 import TripsList from "../components/TripsList";
 
-function Trips(props) {
-  return (
-    <div>
-      <h1 className="font-bold text-2xl mb-4 border-b-2 pb-2 border-green-500">
-        TRIPS
-      </h1>
-      <TripsList />
-    </div>
-  );
+class Trips extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="font-bold text-2xl mb-4 border-b-2 pb-2 border-green-500">
+          TRIPS
+        </h1>
+        <TripsList />
+      </div>
+    );
+  }
 }
 
 export default withAuth(Trips);
