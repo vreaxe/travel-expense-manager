08718488aeb3c1cca6de18d70f62bf5037290e2a
vreaxe <vreaxe@gmail.com>
2019-05-28 23:32:24 +0200

    FAB component

---
+++ clients/web/components/elements/FAB.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Tooltip from "rc-tooltip";
+
+const FAB = props => {
+  return (
+    <div className="fixed" style={props.position}>
+      <Tooltip
+        placement="top"
+        trigger={["hover"]}
+        overlay={props.tooltipContent ? props.tooltipContent : ""}
+        overlayStyle={{
+          opacity: 1,
+          display: typeof props.tooltipContent === "undefined" ? "none" : null
+        }}
+        align={{
+          offset: [0, 2]
+        }}
+      >
+        <div
+          className="flex items-center text-center bg-green-500 hover:bg-green-600 active:bg-green-700 h-16 w-16 rounded-full shadow-lg cursor-pointer"
+          onClick={props.onClick}
+        >
+          {props.children}
+        </div>
+      </Tooltip>
+    </div>
+  );
+};
+
+FAB.propTypes = {
+  position: PropTypes.shape({
+    top: PropTypes.number,
+    bottom: PropTypes.number,
+    left: PropTypes.number,
+    right: PropTypes.number
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+export default FAB;
