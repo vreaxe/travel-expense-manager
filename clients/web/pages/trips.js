0271bae925f766a779905334b448715c994bee72
vreaxe <vreaxe@gmail.com>
2019-06-21 23:01:43 +0200

    Add meta title to trips page

--- clients/web/pages/trips.js
+++ clients/web/pages/trips.js
@@ -1,10 +1,12 @@
 import withAuth from "../lib/withAuth";
 import TripsList from "../components/TripsList";
+import Meta from "../components/layouts/Meta";
 
 class Trips extends React.Component {
   render() {
     return (
       <div>
+        <Meta title={`Trips`} />
         <h1 className="font-bold text-2xl mb-4 border-b-2 pb-2 border-green-500 uppercase">
           TRIPS
         </h1>
