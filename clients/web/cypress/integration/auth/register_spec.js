d729df33509cccf625dae1b663fc8281c276714c
vreaxe <vreaxe@gmail.com>
2019-08-20 23:18:06 +0200

    Register success test

--- clients/web/cypress/integration/auth/register_spec.js
+++ clients/web/cypress/integration/auth/register_spec.js
@@ -12,6 +12,7 @@ describe("Register", () => {
       .type(this.user.password)
       .should("have.value", this.user.password);
     cy.get("form").submit();
+
     cy.get("form").contains("Enter a valid email address.");
   });
 
@@ -23,6 +24,19 @@ describe("Register", () => {
       .type(this.user.wrong_password)
       .should("have.value", this.user.wrong_password);
     cy.get("form").submit();
+
     cy.get("form").contains("The password must have at least 8 characters");
+  });
+
+  it("Register success", function() {
+    cy.get('input[name="email"]')
+      .type(this.user.email)
+      .should("have.value", this.user.email);
+    cy.get('input[name="password"]')
+      .type(this.user.password)
+      .should("have.value", this.user.password);
+    cy.get("form").submit();
+
+    cy.location("pathname").should("eq", "/trips");
   });
 });
