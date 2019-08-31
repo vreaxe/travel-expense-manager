2554f62884ae05e9fb8bcbe73a6fb7f6d78ec268
vreaxe <vreaxe@gmail.com>
2019-08-31 13:30:43 +0200

    Improve update expense mutation

--- backend/backend/graphql/mutations.py
+++ backend/backend/graphql/mutations.py
@@ -5,6 +5,9 @@ from django.core.exceptions import ValidationError as DjangoValidationError
 from backend.graphql.exceptions import PermissionDenied
 from backend.graphql.exceptions import ValidationError
 
+class CustomMutationOptions(MutationOptions):
+    editable_fields = []
+
 # Based on BaseMutation: https://github.com/mirumee/saleor/blob/master/saleor/graphql/core/mutations.py
 class BaseMutation(graphene.Mutation):
     class Meta:
@@ -15,11 +18,14 @@ class BaseMutation(graphene.Mutation):
         cls,
         arguments=None,
         description=None,
+        editable_fields=(),
         _meta=None,
         **options
     ):
         if not _meta:
-            _meta = MutationOptions(cls)
+            _meta = CustomMutationOptions(cls)
+            _meta.editable_fields = editable_fields
+
         super().__init_subclass_with_meta__(_meta=_meta, **options)
 
     @classmethod
