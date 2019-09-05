a8e4fad8106bc42ecb7eb59dec6eb1610d7f82b5
vreaxe <vreaxe@gmail.com>
2019-09-05 20:07:10 +0200

    Add PurgeCSS

--- clients/web/postcss.config.js
+++ clients/web/postcss.config.js
@@ -1,10 +1,42 @@
 const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
 
 module.exports = {
   plugins: [
     require("postcss-easy-import"),
     require("postcss-nested"),
     tailwindcss("./tailwind.config.js"),
+    purgecss({
+      content: ["./components/**/*.js", "./components/*.js", "./pages/*.js"],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ["js"]
+        }
+      ],
+      whitelist: [
+        "html",
+        "body",
+
+        // react-tiny-fab
+        "open",
+        "closed",
+        "right"
+      ],
+      whitelistPatterns: [
+        /rtf/,
+        /react-datepicker/,
+        /react-select/,
+        /rc-tooltip/,
+        /spinner/
+      ]
+    }),
     require("autoprefixer")
   ]
 };
